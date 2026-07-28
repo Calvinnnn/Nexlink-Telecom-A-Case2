262cc8fb4c612c03cdd0f120dfe22c76933587d0
fo2sh21 <moneraldaw544@gmail.com>
2026-07-28 20:15:14 +0300

    simple python script to create or delete and recreate DB, apply schema and sample data for a fresh start

---
+++ db/setup_db.py
@@ -0,0 +1,39 @@
+import sqlite3
+import os
+
+DB_PATH = 'nextlink.db'
+SCHEMA_PATH = 'schema.sql'
+SEED_PATH = 'seed.sql'
+
+def reset_database():
+
+    if os.path.exists(DB_PATH):
+        os.remove(DB_PATH)
+        print("Existing database deleted.")
+    else:
+        print("No existing database found. Creating a new one...")
+
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    try:
+        with open(SCHEMA_PATH, 'r') as schema_file:
+            print("Apply schema")
+            cursor.executescript(schema_file.read())
+
+        with open(SEED_PATH, 'r') as seed_file:
+            print("Applying seed data")
+            cursor.executescript(seed_file.read())
+
+        conn.commit()
+        print("Success: Nextlink database rebuilt and seeded perfectly.")
+
+    except FileNotFoundError as e:
+        print(f"Error: Missing SQL file. Ensure both schema.sql and seed.sql are in the same folder. ({e})")
+    except sqlite3.Error as e:
+        print(f"Database Error: {e}")
+    finally:
+        conn.close()
+
+if __name__ == "__main__":
+    reset_database()
